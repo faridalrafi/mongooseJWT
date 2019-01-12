cb353f50d44a45d2134dfc080260296273c41235
faridalrafi <faridalrafi@gmail.com>
2019-01-12 12:03:09 +0700

    test ok

--- controllers/user.js
+++ controllers/user.js
@@ -55,21 +55,21 @@ exports.user_create = function (req, res, next) {
   }
 }
 
-exports.user_details = function (req, res) {
+exports.user_details = function (req, res, next) {
   User.findById(req.params.id, function (err, user) {
     if (err) return next(err);
     res.send(user);
   })
 };
 
-exports.user_update = function (req, res) {
+exports.user_update = function (req, res, next) {
   User.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err, user) {
     if (err) return next(err);
     res.send('User updated.');
   });
 };
 
-exports.user_delete = function (req, res) {
+exports.user_delete = function (req, res, next) {
   User.findByIdAndRemove(req.params.id, function (err) {
     if (err) return next(err);
     res.send('User Deleted successfully!');
