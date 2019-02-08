2779ee951e66a5050f6f40be73f24c404b454486
faridalrafi <faridalrafi@gmail.com>
2019-02-08 19:31:08 +0700

    all

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var auth = require('../middleware/auth')
 const user_controller = require('../controllers/user')
 
-router.get('/test',auth.isAuthenticated, user_controller.test)
+router.get('/test', auth.isAuthenticated, user_controller.test)
 router.post('/create', user_controller.user_create)
 router.get('/all', user_controller.user_all)
 router.put('/:id/update', user_controller.user_update)
 router.delete('/:id/delete', user_controller.user_delete)
 router.post('/login', user_controller.authentication)
+router.get('/detail/:id', user_controller.user_details);
 module.exports = router;
