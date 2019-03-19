1205fdbe51b30ee2cc122d7d1bea397a51c20a50
user <user@users-MacBook-Pro.local>
2019-03-20 02:33:27 +0700

    refactor router

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,17 @@ const userController = require('../controllers/user')
 router.get('/test', auth.isAuthenticated, userController.Test)
 router.get('/testsend', auth.isAuthenticated, userController.TestSend)
 router.get('/testend', auth.isAuthenticated, userController.TestEnd)
-
-router.post('/create', userController.UserCreate)
-router.get('/all', userController.UserAll)
+// POST user
+router.post('/', userController.UserCreate)
+// GET user
+router.get('/', userController.UserAll)
+// GET user
 router.get('/allPromise', userController.UserAllPromise)
+// Update user
 router.put('/:id/update', userController.UserUpdate)
+// Delete user
 router.delete('/:id/delete', userController.UserDelete)
+// GET user detail
 router.get('/detail/:id', userController.UserDetail);
 
 module.exports = router;
